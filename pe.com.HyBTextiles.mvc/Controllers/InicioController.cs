0faafff0a1c976035216c93735cae9f678d009f0
Diego Lora <diegoalejandro1094@gmail.com>
2026-07-24 21:09:15 -0500

    Vistas CRUD de las 30 entidades + menu MVC + fix Rol y ruta por defecto (#1)
    * Vistas CRUD de las 30 entidades + menu MVC + fix Rol y ruta por defecto
    
    * fix login

--- pe.com.HyBTextiles.mvc/Controllers/InicioController.cs
+++ pe.com.HyBTextiles.mvc/Controllers/InicioController.cs
@@ -11,8 +11,27 @@ namespace pe.com.HyBTextiles.mvc.Controllers
     {
         // GET: Inicio
         ApplicationDbContext db = new ApplicationDbContext();
+        [HttpGet]
         public ActionResult Index()
         {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Index(string txtUsu, string txtCla)
+        {
+            var usuario = db.usuario
+                .FirstOrDefault(u => u.userusu == txtUsu
+                                  && u.claveusu == txtCla
+                                  && u.estusu);
+            if (usuario != null)
+            {
+                Session["usuario"] = usuario.nomusu;
+                Session["codusu"] = usuario.codusu;
+                Session["codrol"] = usuario.codrol;
+                return RedirectToAction("Index", "Menu");
+            }
+            ViewBag.error = "Usuario o clave incorrectos";
             return View();
         }
     }
